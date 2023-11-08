155a7b8ac6e8ce2bf4f91815b95ad9de796c633a
RumonHasan <rumonuniv@gmail.com>
2023-11-09 00:58:14 +0900

    File uploader setup

---
+++ src/components/shared/FileUploader.tsx
@@ -0,0 +1,56 @@
+import { useCallback, useState } from 'react';
+import { useDropzone, FileWithPath } from 'react-dropzone';
+import { Button } from '../ui/button';
+
+type FileUploaderProps = {
+  fieldChange: (FILES: File[]) => void;
+  mediaUrl: string;
+};
+
+const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
+  const [file, setFile] = useState<File[]>([]);
+  const [fileUrl, setFileUrl] = useState('');
+
+  const onDrop = useCallback(
+    (acceptedFiles: FileWithPath[]) => {
+      setFile(acceptedFiles);
+      fieldChange(acceptedFiles);
+      setFileUrl(URL.createObjectURL(acceptedFiles[0]));
+    },
+    [fieldChange]
+  );
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: {
+      'image/*': ['.png', '.jpeg', '.jpg', '.svg'],
+    },
+  });
+
+  return (
+    <div
+      {...getRootProps()}
+      className="flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer "
+    >
+      <input {...getInputProps()} className="cursor-pointer" />
+      {fileUrl ? (
+        <div></div>
+      ) : (
+        <div className="file_uploader-box">
+          <img
+            src="/assets/icons/file-upload.svg"
+            width={96}
+            height={77}
+            alt="fileupload"
+          />
+          <h3 className="base-medium text-light-2 mb-2 mt-6">
+            Drag your photo here boi!!
+          </h3>
+          <p className="text-light-4 small-regular mb-6">PNG, JPG</p>
+          <Button className="shad-button_dark_4">Add From Your Machine</Button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FileUploader;
